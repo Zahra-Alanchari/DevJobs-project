c4d2735f889244a2ddfdadf25be09a1266364452
Zahra Alanchari <zahraalanchari@Zahras-MacBook-Pro.local>
2024-07-15 01:26:04 +0330

    webpack setup

---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+export default function Main(){
+    return <div>
+        <h1>hi</h1>
+    </div>
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css'
+import Main from './components/Main';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello, React with Webpack!!</h1>
+      <h1>hey</h1>
     </div>
   )
 }
