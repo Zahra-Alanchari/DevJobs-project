0027bcd710d13df1c427b2da6e9d31a2b9e69fe0
Zahra Alanchari <zahraalanchari@Zahras-MacBook-Pro.local>
2024-07-31 19:53:18 +0330

    clear empty file

--- src/redux/Slice.js
+++ src/redux/Slice.js
@@ -1,5 +1,5 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import getJob from "../jobsApi";
+import { createSlice } from "@reduxjs/toolkit";
+
 import { fetchgetAllJobs } from "./action";
 
 const initialState = {
